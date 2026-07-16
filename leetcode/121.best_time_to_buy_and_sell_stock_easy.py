e9a7bf63dc0bb14ccce6868024c0eba0585545dd
Christina <cristi828@gmail.com>
2026-07-16 17:24:28 +0300

    fix: cleaned solution 121. Best Time to Buy and Sell Stock

--- leetcode/121.best_time_to_buy_and_sell_stock_easy.py
+++ leetcode/121.best_time_to_buy_and_sell_stock_easy.py
@@ -4,17 +4,15 @@ from typing import List
 def maxProfit(prices: List[int]) -> int:
     profit = 0
     minCost = prices[0]
-    minCostIndex = 0
 
     for i, value in enumerate(prices):
         future_profit = value - minCost
 
-        if i > minCostIndex and value > minCost and future_profit > profit:
+        if future_profit > profit:
             profit = future_profit
 
         if value < minCost:
             minCost = value
-            minCostIndex = i
 
     return profit
 
